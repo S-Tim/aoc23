2231f171ddeb7c0d4144c4fb07ead277b2e320af
Tim Silhan <tim.silhan@gmail.com>
2023-10-20 11:25:17 +0200

    Add script for creating templates for each day

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.math.max
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,53 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
+    return Pair(first + other.first, second + other.second)
+}
+
+typealias Point = Pair<Int, Int>
+
+/**
+ * Prints a collection of points as a grid.
+ *
+ * @param points the points that define the grid
+ * @param padding extra space to add to the top, bottom, left and right of the grid
+ * @param occupiedSymbol symbol used for the grid cells where the points are
+ * @param emptySymbol symbol used for grid cells without a point
+ * @param printRowNumbers indicates whether line numbers should be printed
+ */
+fun printAsGrid(
+    points: Collection<Point>,
+    padding: Int = 0,
+    occupiedSymbol: Char = '#',
+    emptySymbol: Char = '.',
+    printRowNumbers: Boolean = false
+) {
+    val minX = points.minBy { it.first }.first - padding
+    val maxX = points.maxBy { it.first }.first + padding
+    val minY = points.minBy { it.second }.second - padding
+    val maxY = points.maxBy { it.second }.second + padding
+
+    val rowNumberLength = max(minY.toString().length, maxY.toString().length)
+    val columnNumberLength = max(minX.toString().length, maxX.toString().length)
+
+    if (printRowNumbers) {
+        val columnNumbers = (minX..maxX).map { it.toString().padStart(columnNumberLength) }
+        for (i in 0 until columnNumberLength) {
+            print(" ".repeat(rowNumberLength + 1) + columnNumbers.map { it[i] }.joinToString(""))
+            println()
+        }
+    }
+
+    for (y in minY..maxY) {
+        if (printRowNumbers) {
+            print(y.toString().padStart(rowNumberLength) + " ")
+        }
+        for (x in minX..maxX) {
+            if (Point(x, y) in points) print(occupiedSymbol) else print(emptySymbol)
+        }
+        println()
+    }
+    println()
+}
