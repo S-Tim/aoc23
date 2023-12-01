6131a9df06eb79dea37e0786eb043304de0c9d3b
Tim Silhan <tim.silhan@gmail.com>
2023-12-01 14:38:32 +0100

    day1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,19 +4,68 @@ import println
 import readInput
 
 fun main() {
+    fun firstAndLastDigit(calibrationValue: String, replacementValues: List<Pair<String, Int>>): Int {
+        val firstNumberString =
+            replacementValues.map { it.second to calibrationValue.indexOf(it.first) }.filter { it.second != -1 }
+                .minBy { it.second }
+        val lastNumberString =
+            replacementValues.map { it.second to calibrationValue.lastIndexOf(it.first) }.filter { it.second != -1 }
+                .maxBy { it.second }
+
+        return Integer.parseInt(firstNumberString.first.toString() + lastNumberString.first.toString())
+        // 8954bxsqntndjmonenx5
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = listOf(
+            "1" to 1,
+            "2" to 2,
+            "3" to 3,
+            "4" to 4,
+            "5" to 5,
+            "6" to 6,
+            "7" to 7,
+            "8" to 8,
+            "9" to 9,
+        )
+
+        return input.sumOf { firstAndLastDigit(it, numbers) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = listOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+            "1" to 1,
+            "2" to 2,
+            "3" to 3,
+            "4" to 4,
+            "5" to 5,
+            "6" to 6,
+            "7" to 7,
+            "8" to 8,
+            "9" to 9
+        )
+
+        return input.sumOf { firstAndLastDigit(it, numbers) }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    // test if implementation meets criteria from the description, like:
+    val testInput1 = readInput("day01/day01_1_test")
+    check(part2(testInput1) == 142)
+
+    val testInput2 = readInput("day01/day01_test")
+    check(part2(testInput2) == 281)
+
+    val input = readInput("day01/day01")
     part1(input).println()
     part2(input).println()
 }
